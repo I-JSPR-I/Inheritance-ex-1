acb81769d277572828943a0b151e1f6895e7f91f
I-JSPR-I <jaspermeersschaut@hotmail.com>
2022-04-26 10:01:08 +0200

    Add project files.

---
+++ Inheritance ex 1/AangetekendeBrief.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance_ex_1
+{
+    class AangetekendeBrief
+    {
+        public double Reisafstand;
+        public byte Reistijd;
+        public double Kostprijs;
+
+        public void reistijd()
+        {
+            Console.WriteLine($"De reistijd is {Math.Round(Reisafstand / 100)} dagen");
+        }
+        public void kostprijs()
+        {
+                int i = -1;
+                do
+                {
+                    Reisafstand = Reisafstand - 100;
+                    i++;
+                } while (Reisafstand > 0);
+                Kostprijs = 15 + (10 * i);
+            
+        }
+    }
+}
